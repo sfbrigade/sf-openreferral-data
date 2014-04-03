d3b94cdc282f0ee8cecb524a455a15a214a95642
Adam Fishman <fishman.adam@gmail.com>
2014-04-03 08:39:17 -0700

    Finished contacts parser

--- ss4women/parser_csv_to_json.py
+++ ss4women/parser_csv_to_json.py
@@ -1,5 +1,6 @@
 import re
 from csv import reader
+
 f = open('contacts.csv');  g = open('split_contacts.csv', 'w+')
 def parse_contact_field(field, g):
     field = field.strip()
@@ -10,37 +11,44 @@ def parse_contact_field(field, g):
         if re.search(r'\.org|On website|Receptionist', elem[0]):
             continue
         else:
-            person = {
-                'name': elem[0].strip(),
-            }
-            if len(elem) > 1:
-                for i, section in enumerate(elem):
-                    if re.search(r'@', section):
-                        person['email'] = section.strip()
-                    if re.search(r'\(\d{3}\)\ \d{3}-\d{4}', section):
-                        match = re.search(r'\(\d{3}\)\ \d{3}-\d{4}', section).group()
-                        person['phone'] = match
-                        if i == 1:
-                            section = re.sub(r'\(\d{3}\)\ \d{3}-\d{4}', r'', section)
-                            person['title'] = section.strip()
-                    if re.search(r'x\d+|ex...\d+|EX \d+', section):
-                        match = re.search(r'x\d+|ex...\d+|EX \d+', section).group()
-                        person['extension'] = 'x' + re.search(r'\d+', match).group()
-                        if i == 1:
-                            section = re.sub(r'x\d+|ex...\d+|EX \d+', r'', section)
-                            person['title'] = section.strip()
-                if person.get('title','') == '':
+            if re.search(r'\(\d{3}\).\d{3}.\d{4}', elem[0]):
+                match = re.search(r'\(\d{3}\).\d{3}.\d{4}', elem[0]).group()
+                subbed = re.sub(r'\(\d{3}\).\d{3}.\d{4}', r'', elem[0])
+                subbed = re.sub(r'\(ACCESS\):|\(TAP\):', r'', subbed)
+                person = {
+                        'phone': match.strip(),
+                        'name': subbed.strip()
+                        }
+            else:
+                person = {
+                    'name': elem[0].strip(),
+                }
+            for i, section in enumerate(elem):
+                if re.search(r'@', section):
+                    person['email'] = section.strip()
+                if re.search(r'\(\d{3}\).\d{3}.\d{4}', section):
+                    match = re.search(r'\(\d{3}\).\d{3}.\d{4}', section).group()
+                    person['phone'] = match
+                    if i == 1:
+                        section = re.sub(r'\(\d{3}\).\d{3}.\d{4}', r'', section)
+                        person['title'] = section.strip()
+                if re.search(r'x\d+|ex...\d+|EX \d+', section):
+                    match = re.search(r'x\d+|ex...\d+|EX \d+', section).group()
+                    person['extension'] = 'x' + re.search(r'\d+', match).group()
+                    if i == 1:
+                        section = re.sub(r'x\d+|ex...\d+|EX \d+', r'', section)
+                        person['title'] = section.strip()
+            if person.get('title','') == '' and len(elem)>1:
+                if re.search('RPN|LCSW', elem[1]) and len(elem) == 2:
+                    pass
+                elif re.search(r'Ph\.D\.|M\.S\.W\.|LCSW', elem[1]):
+                    person['title'] = elem[2].strip()
+                else:
                     person['title'] = elem[1].strip()
 
-
         contacts.append(person)
-    g.write(str(contacts) + '\n')
+    return contacts
 
-for line in f:
-    if line.strip(' \n'):
-        g.write(line.strip() + '\n')
-
-g.close()
 
 def parse(read):
     orgs = {}
