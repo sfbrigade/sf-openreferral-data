11e19ba820a1fb187d3598cb945a121dbb3ffd5b
Scott Lau <scott@change.org>
2014-04-23 18:59:02 -0700

    Fixed a mistake in the structure of the json

--- parsers/asian_womens_shelter_parser.rb
+++ parsers/asian_womens_shelter_parser.rb
@@ -2,22 +2,21 @@
 require 'json'
 
 module Parser
-  def self.run
-    puts AsianWomensShelter.parse()
-  end
-
   # I couldn't find much data being listed here,
   # so this will just spit out the resource data for the Asian Womens Shelter
   class AsianWomensShelter
-    # DATA = <<-ADDRESS
-    #     Asian Women's Shelter
-    #     3543 18th Street # 19
-    #     San Francisco, CA 94110
+    def self.fetch_data
+      # no-op
+      # DATA = <<-ADDRESS
+      #     Asian Women's Shelter
+      #     3543 18th Street # 19
+      #     San Francisco, CA 94110
 
-    #     Business Line: (415) 751-7110
-    #     Fax: (415) 751-0806
-    #     info@sfaws.org
-    # ADDRESS 
+      #     Business Line: (415) 751-7110
+      #     Fax: (415) 751-0806
+      #     info@sfaws.org
+      # ADDRESS 
+    end
 
     def self.parse
       {
@@ -39,13 +38,11 @@ module Parser
           ],
           faxes: [
             {number: "(415) 751-0806"}
-          ]
-        },
-        description: "Services include our nationally recognized shelter program, language advocacy program, crisis line, case management, and programs in support of underserved communities such as queer Asian survivors and trafficked survivors. Women's Program",
-        short_description: "Shelter program, language advocacy program, crisis line, case management"
-      }.to_json
+          ],
+          description: "Services include our nationally recognized shelter program, language advocacy program, crisis line, case management, and programs in support of underserved communities such as queer Asian survivors and trafficked survivors. Women's Program",
+          short_description: "Shelter program, language advocacy program, crisis line, case management"
+        }
+      }
     end
   end
 end
-
-Parser.run()
