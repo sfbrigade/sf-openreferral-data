d37f84e149461ac0bfdfaf99f2c771e1a167c890
Jesse Aldridge <jessealdridge@gmail.com>
2014-06-18 20:40:07 -0700

    pull info out of glide html

---
+++ glide/parse_glide.py
@@ -0,0 +1,41 @@
+
+import re
+
+from nltk.tag import pos_tag
+
+def find_phone_numbers():
+  with open('glide_contact_page.txt') as f:
+    contact_text = f.read()
+  no_tags = re.sub('<.*?>', ' ', contact_text)
+  words = [word.strip() for word in re.split('\s+', no_tags)]
+  for word in words:
+    if re.search('[0-9]{3}.[0-9]{3}.[0-9]{4}', word):
+      yield word
+
+def find_name():
+  with open('glide.txt') as f:
+    front_text = f.read()
+
+  no_tags = re.sub('<.*?>', '', front_text)
+  no_tags = [word.strip().lower() for word in no_tags.split(' ') if word]
+
+  word_counts = {}
+  for word in no_tags:
+    word_counts.setdefault(word, 0)
+    word_counts[word] += 1
+  for word, count in sorted(
+    word_counts.iteritems(), key=lambda x: -x[1])[:20]:
+    if len(word) > 3:
+      return word
+
+print 'name:', find_name()
+print 'phone numbers:', list(find_phone_numbers())
+
+
+# print 'tagging'
+# tagged_sent = pos_tag(words[:100])
+# print 'tagged'
+
+# propernouns = [word for word,pos in tagged_sent if pos == 'NNP']
+# print propernouns
+# # ['Michael','Jackson', 'McDonalds']
